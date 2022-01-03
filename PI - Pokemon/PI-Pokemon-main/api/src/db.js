41f8d9c990e2c9fe362ceb8a9f79462e3b276dab
solpatrone <sol.patrone@hotmail.com>
2022-01-03 15:58:04 -0300

    PRUEBA

--- PI - Pokemon/PI-Pokemon-main/api/src/db.js
+++ PI - Pokemon/PI-Pokemon-main/api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
-
+//HACIENDO UNA PRUEBA DE GIT
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
   {
